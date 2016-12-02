99cbc906894b8a179f73c7eb2f19e083fa1a5a5b
Rohitash Tulyani <rtulyani@sapient.com>
2016-12-02 22:41:07 +0530

    GREEN - Modify code for handling testcase

--- src/main/com/cleancode/numtotext/CreateText.java
+++ src/main/com/cleancode/numtotext/CreateText.java
@@ -5,11 +5,17 @@ public class CreateText{
     String[] textArray = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
             "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
 
+    final String twenty = "twenty";
+    final int value_ten = 10;
+    final int value_twenty = 20;
+
     public String numberToText(int number){
-        if(number <=20){
+        if(number <= value_twenty){
             return textArray[number];
         } else {
-            return "twenty one";
+            number = number % value_ten;
+            return twenty + " " + textArray[number];
         }
     }
+
 }
