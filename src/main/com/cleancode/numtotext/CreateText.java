5b3e99fc19a0559043e5eaa76a286df132affa58
Rohitash Tulyani <rtulyani@sapient.com>
2016-12-03 00:12:19 +0530

    GREEN - Modify code for handling testcase

--- src/main/com/cleancode/numtotext/CreateText.java
+++ src/main/com/cleancode/numtotext/CreateText.java
@@ -10,12 +10,15 @@ public class CreateText{
     final String twenty = "twenty";
     final int value_ten = 10;
     final int value_twenty = 20;
+    final int value_hundred = 100;
 
     public String numberToText(int number){
         if(number <= value_twenty){
             return generateTextIfNUmberIsLessThan20(number);
+        } else if(number < value_hundred) {
+            return generateTextIfNUmberIsLessThan100(number);
         } else {
-            return generateTextIfNUmberIsGreaterThan20(number);
+            return generateTextIfNUmberIsGreaterThan100(number);
         }
     }
 
@@ -23,7 +26,7 @@ public class CreateText{
         return textArray[number];
     }
 
-    private String generateTextIfNUmberIsGreaterThan20(int number){
+    private String generateTextIfNUmberIsLessThan100(int number){
         return getTensDigitText(number) + " " + getUniqueDigitText(number);
     }
 
@@ -33,5 +36,9 @@ public class CreateText{
 
     private  String getUniqueDigitText(int number){
         return textArray[number % value_ten];
+    }
+
+    private String generateTextIfNUmberIsGreaterThan100(int number){
+        return textArray[number/value_hundred] + " " + "hundred" + " " + numberToText(number%value_hundred);
     }
 }
